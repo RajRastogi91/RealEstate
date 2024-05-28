4a38bbd255ab8937da55b6f9174dd8094c18c90e
Gurjeet_Gonia <gurjeet@perceptsystems.com>
2024-05-28 12:57:01 +0530

    updated payment url

--- Server/Controllers/payment.controller.js
+++ Server/Controllers/payment.controller.js
@@ -35,8 +35,8 @@ export const createsession = async (req, res) => {
           payment_method_types: ['card'],
           line_items: lineItems,  
           mode: 'payment',
-          success_url: `http://localhost:5173/Success?session_id={CHECKOUT_SESSION_ID}&user_id=${user_id}&amount=${amount}&property_id=${property_id}`,
-          cancel_url: "http://localhost:5173/Cancel",
+          success_url: `https://squareproperty.netlify.app/Success?session_id={CHECKOUT_SESSION_ID}&user_id=${user_id}&amount=${amount}&property_id=${property_id}`,
+          cancel_url: "https://squareproperty.netlify.app/Cancel",
           customer: customer.id
          
       });
